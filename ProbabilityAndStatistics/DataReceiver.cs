b3183749f552c67128a107a63d4c48efa3d012c0
Furkan-Bilici <fbilici258@gmail.com>
2025-03-11 14:35:34 +0300

    Veri alma işlemi sağlandı

---
+++ ProbabilityAndStatistics/DataReceiver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ProbabilityAndStatistics
+{
+    internal class DataReceiver
+    {
+         
+        public List<float> floatList = new List<float>();
+        public void SetData()
+        {
+            List<string> Data=new List<string>();
+            Console.WriteLine("Verileri girin(çıkış için q'yazın)");
+            while (true)
+            {
+                
+                string data = Console.ReadLine();
+                if (data == "q")
+                    break;
+                Data.Add(data);
+            }
+            ConvertToFloatList(Data);
+        }
+        public void GetData()
+        {
+            Console.WriteLine("Dönüştürülen float listesi: " + string.Join("-", floatList));
+        }
+   
+    void ConvertToFloatList(List<string> Data)
+        {
+            foreach (var item in Data)
+            {
+                if (float.TryParse(item, out float number))
+                {
+                    floatList.Add(number);
+                }
+                else
+                {
+                    Console.WriteLine($"Geçersiz giriş atlandı: {item}");
+                }
+            }
+        }
+    }
+}
--- ProbabilityAndStatistics/Program.cs
+++ ProbabilityAndStatistics/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ProbabilityAndStatistics;
+
+DataReceiver receiver = new DataReceiver();
+receiver.SetData();
+receiver.GetData();
